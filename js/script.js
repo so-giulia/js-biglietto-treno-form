1f889c79b3494509bfff448db5957c83cc76c7cf
GiuliaSoccolini <giuliasoccolini@yahoo.it>
2021-06-05 19:24:01 +0200

    add: programma (3za parte)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 //Creiamo un finto biglietto del treno con:
-//Nome passeggero OK
-//Codice treno (numero casuale tra 90000 e 100000 escluso)
-//Numero carrozza (numero casuale tra 1 e 9 incluso)
+//**OK** Nome passeggero
+//**OK** Codice treno (numero casuale tra 90000 e 100000 escluso)
+//**OK** Numero carrozza (numero casuale tra 1 e 9 incluso)
+
 //Prezzo calcolato in base all'esercizio visto in precedenza
 //Categoria selezionata dall'utente
-//Al click su "Genera" appare la schermata con le caratteristiche del biglietto. OK
-//Al click su "Annulla" dobbiamo ripulire il form OK
+
+//**OK** Al click su "Genera" appare la schermata con le caratteristiche del biglietto.
+//**OK** Al click su "Annulla" dobbiamo ripulire il form 
+
 //Bonus: aggiungete qualche controllo sui dati di input!
 
 
@@ -22,12 +25,10 @@ genera.addEventListener("click", function(){
 
     //prendo i valori
     var nome = document.getElementById("nome").value;
-    var cognome = document.getElementById("cognome").value;
-    var age = document.getElementById("age").value;
+    var km = document.getElementById("km").value;
 
     //li visualizzo nel biglietto
     document.getElementById("nomeOutput").innerHTML = nome;
-    document.getElementById("nomeOutput").innerHTML += "<br>" + cognome;
 
     //——————————————————————————
     //aggiungo gli altri valori:
@@ -41,6 +42,22 @@ genera.addEventListener("click", function(){
     var carrozza = Math.round(Math.random() * (9 - 1) + 1);
     document.getElementById("carrozzaOutput").innerHTML = carrozza;
 
+    //calcolo prezzo in base all'età
+    var prezzo = km * 0.21;
+    var age = document.getElementById("age").selectedIndex;
+
+    if(age == "0"){
+        prezzo = parseFloat((prezzo - (prezzo * 0.2)).toFixed(2));
+        document.getElementById("offertaOutput").innerHTML = "Sconto minorenni";
+        document.getElementById("costoOutput").innerHTML = prezzo + "€";
+    }else if(age == "2"){
+        prezzo = parseFloat((prezzo - (prezzo * 0.4)).toFixed(2));
+        document.getElementById("offertaOutput").innerHTML = "Sconto over 65";
+        document.getElementById("costoOutput").innerHTML = prezzo + "€";
+    }else{
+        document.getElementById("offertaOutput").innerHTML = "Nessuna offerta";
+        document.getElementById("costoOutput").innerHTML = prezzo + "€";
+    }
 });
 
 
@@ -51,7 +68,6 @@ genera.addEventListener("click", function(){
 var annulla = document.getElementById("annulla");
 
 annulla.addEventListener("click", function(){
-
     //nascondo il biglietto
     document.getElementById("output").className = "hide";
 });
